7e1ffcbab7d01fdaa2074c324d159de766821f04
Tyler Jerman <Tyler.s.jerman@gmail.com>
2023-08-03 11:46:41 -0600

    update

--- src/components/ContainerFluidExample.jsx
+++ src/components/ContainerFluidExample.jsx
@@ -15,15 +15,14 @@ for(let i=0; i<amountOfRows; i++) {
 
   return (
     <Container fluid>
-    <Row>
-      <Col className= "col" xs="9">Column width 9</Col>
-      <Col className= "col" xs="3">Column width 3</Col>
-    </Row>
-    <Row>
-      <Col className="col" xs="6">Column</Col>
-      <Col className= "col" xs="6">Fractions and such that require many different things like Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quibusdam hic distinctio, quisquam tempore consequatur magni nemo similique voluptas? Nostrum, incidunt! Commodi, consectetur accusamus? Aspernatur incidunt accusantium rem praesentium vel!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, illum? Neque excepturi perferendis voluptates repellendus cum, saepe earum, at, accusamus consequatur dolorem laborum? Maxime, quos. Enim ullam voluptas labore earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dicta, deleniti, nobis deserunt, maiores dolorum nihil temporibus explicabo reprehenderit facilis minima doloribus dolorem eveniet. Voluptatibus ad officia excepturi nisi esse?</Col>
-      <Col className="col" xs="12">Column</Col>
-    </Row>
+      <Row>
+        <Col className="col" xs="12" sm="6" md="6" lg="3">
+          Sidebar
+        </Col>
+        <Col className="col" xs="12" sm="6" md="6" lg="9">
+          Main Content
+        </Col>
+      </Row>
   </Container>
   );
 }
